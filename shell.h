944194f6f42dfac17ce097521cdd30b49927a9ff
Munoru101 <dennismunoru@gmail.com>
2023-11-17 18:12:36 -0800

    Handled implicit errors

--- shell.h
+++ shell.h
@@ -11,14 +11,17 @@
 int _strcmp(char *s1, char *s2);
 char *_strcpy(char *dest, char *src);
 char *find_path(char *command);
+char *search_path(char *command);
 char **_split(char *str, char *sep);
 char *_strcat(char *dest, char *src);
 int _strlen(char *s);
 char *_getenv(char *var);
 void *_calloc(unsigned int nmemb, unsigned int size);
 void _env(void);
+void print_env(void);
 int execute(char **args);
 int empty_line(char *buff);
+int no_line(char *buff);
 
 extern char **environ;
 #endif
